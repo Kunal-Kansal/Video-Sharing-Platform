55a90e12ec013738abdbbaa810559bc8a9df63bb
Kunal-Kansal <1kunalkansal@gmail.com>
2023-04-22 07:05:54 +0530

    the files are updated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './App.css';
 import NavLoginBar from './navigationBar/navLoginBar';
 import NavLoginBar2 from './navigationBar/navLoginBar2';
+import SignInPage from './SignInPage';
+import RegisterPage from './RegisterPage';
+import UploadNewVideo from './uploadNewVideo';
+import { BrowserRouter, Route, Switch,Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-    <NavLoginBar/>
-    {/* <NavLoginBar2/> */}
-      hi
+      <BrowserRouter>
+      <Routes>
+        <Route path='/SignInPage' element={<><Nav/><SignInPage/></>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
